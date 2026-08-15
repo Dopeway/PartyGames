f8f68a9ddb9316dda8a3eadc08b6abb2f9bdc4e5
Mehdi BEN HAMIDA <benhamimehdi7@gmail.com>
2026-08-15 15:39:03 +0100

    chore: initial commit with GitHub Pages setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/PartyGames',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
